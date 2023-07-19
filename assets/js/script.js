e89bade6f088e788237f9d22869f8190f329a370
Sarah Wesley <srwesley722@gmail.com>
2023-07-18 21:59:46 -0500

    Changes to index and adding incomplete code to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,56 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDay = dayjs();
+$('#currentDay').text(currentDay.format('dddd, MMMM D YYYY'));
+
 $(function () {
+  
+  var currentHour = parseInt(moment().format('h'));
+  
+  function hourColor() {
+    $('.time-block').each(function() {
+      var blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
+    });
+  }
+  
+  function enterText() {
+    $('.saveBtn').on('click', function () {
+      var key = $(this).parent().attr('id');
+      var value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
+  
+  function refreshColor() {
+    $('.time-block').each(function() {
+      var blockHour = parseInt(this.id);
+      if (blockHour === currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
+  $('.time-block').each(function() {
+    var key = $(this).attr('id');
+    var value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+
+  hourColor();
+  enterText();
+  refreshColor();
+
+
   // TODO: Add a listener for click events on the save button. This code should
+
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
