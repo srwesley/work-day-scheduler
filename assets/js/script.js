01e3d246a749dd3b8a4f2e40a6e9dd4bbdb784b3
Sarah Wesley <srwesley722@gmail.com>
2023-07-19 16:44:38 -0500

    Made final adjustments and added comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,30 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Sets up a variable currentDay and sets it as the day, and then applies that to in a specified format to the page in the p attribute with id #currentDay
 var currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd, MMMM D YYYY'));
 
+// The overall jQuery DOM
 $(document).ready(function () {
 
+  // This tells the page to save the information typed into the classes .description upon clicking the save button
   $('.saveBtn').on('click', function () {
     var value = $(this).siblings('.description').val();
     var key = $(this).parent().attr('id');
-
+    
+    // This sets whatever typed into the description fields to the local storage
     localStorage.setItem(key, value);
   });
 
+  //This is the function to overall control tracking the time of day, as well as applying new classes to the html to apply the background coloring depending on if it's a past, current, or future time 
   function timeTracker () {
-    var currentTime = dayjs().get('hour');
 
+    // This is the variable that gets the hour of the day
+    var currentTime = dayjs().hour();
+
+    // This selects items with the class .time-block and further selects items that have an "hour-" in the div id of the time blocks
     $('.time-block').each(function() {
-      var blockTime = parseInt($(this).attr('id').split('hour')[1]);
+      var blockTime = parseInt($(this).attr('id').split('hour-')[1]);
 
+      // This if/else statement dictates that depending on which hour of the day compares to the block time in the div id, that they need to remove and add class elements to effect the background color styling
       if (blockTime < currentTime) {
         $(this).removeClass('future');
         $(this).removeClass('present');
@@ -35,6 +41,7 @@ $(document).ready(function () {
     });
   }
 
+  // This keeps and gets the items typed into the descriptions from the local storage to the page upon refreshing the page on the browser, using their saved status
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -45,25 +52,7 @@ $(document).ready(function () {
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
+  // This just calls the timeTracker function to act
   timeTracker();
-  
-  // TODO: Add a listener for click events on the save button. This code should
 
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
